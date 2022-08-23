3b5ae6c14b0c924aca8246d860c0fc7f8f2334b3
Irene-24 <okaforirenen@gmail.com>
2022-08-23 20:54:00 +0100

    api routes fix

--- middleware.ts
+++ middleware.ts
@@ -12,11 +12,14 @@ export async function middleware(request: NextRequest) {
     );
 
     //return 401/403 response for api routes
-    //there will be a handler for the api route, that will extract
+    //there should be a handler for the api route, that will extract
     //url params and pass it along
+    //don't forget to add the protected api routes to matcher
 
     if (request.nextUrl.href.includes("/api")) {
-      return NextResponse.rewrite("/api/error");
+      return NextResponse.rewrite(
+        new URL("/api/error", request.nextUrl.origin)
+      );
     }
 
     //redirect for unauth pages
